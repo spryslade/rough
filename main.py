f0723c7ec7cf692d5f33fa3d990f2d63f8968184
S L A D E <sladexsarcasm@gmail.com>
2022-11-13 14:38:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+buttons = [
+    [
+        InlineKeyboardButton(
+            text="🫶 Add Me To Your Groups",
+            url="t.me/ErikaProBot?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(
+            text=" 🏥 Support", url=f"https://t.me/AstorSupport",
+        ),
+        InlineKeyboardButton(
+            text=" 🎉 Updates", url=f"https://t.me/AstorPro",
+        ),
+        InlineKeyboardButton(
+            text=" 📮 Logs", url=f"https://t.me/ErikaLogs"),
+    ],
+    [
+        InlineKeyboardButton(
+            text=" 🌻 Chat", url=f"https://t.me/ScoutXLegion",
+        ),
+        InlineKeyboardButton(
+            text=" 🔐 Help ", callback_data="help_back"),
+    ],
+  ]
